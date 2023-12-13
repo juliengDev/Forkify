eff4a6d6e7b5dea64046ff7504c7d949cb8b43e5
Julien Gilbert <julien@CodeBox.local>
2023-12-13 17:23:27 +0100

    Add fetching functions

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,27 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const showRecipes = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc990'
+    );
+    const data = await res.json();
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+    if (!res.ok) throw new Error(`${data.message} (status : ${res.status})`);
+  } catch (err) {
+    alert(err);
+  }
+};
+showRecipes();
